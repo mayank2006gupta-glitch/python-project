8b7e5e46a6a7d2218f14b0cdf817845960ddbbdd
mayank <mayank.2006.gupta@gmail.com>
2026-07-02 20:07:59 +0530

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -4,9 +4,10 @@ a=0
 guesses = 0
 while(a!=n):
     guesses +=1
-    a = int(input("enter a no.: "))
+    a = int(input("enter a no. between 1 to 100: "))
     if(a>n):
         print("enter small no.: ")
     else:
         print("enter greater no.: ")    
-print(f"you have guessed the no. {n} in {guesses} attempts ")        
+print(f"you have guessed the no. {n} in {guesses} attempts ") 
+print('Thankyou for playing the game')
